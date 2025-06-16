bdb278686b9196644068f576751e00d05b5e30a3
Bruno Gomes <bsgomes16@gmail.com>
2025-06-16 18:11:50 -0300

    feat: adição de injecoes de dependecia

---
+++ HelpMEI.Infrastructure/Configurations/ServiceCollectionExtensions.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace HelpMEI.Infrastructure.Configurations;
+public static class ServiceCollectionExtensions {
+	public static IServiceCollection AddInfrastructure(this IServiceCollection services) {
+		return services;
+	}
+}
